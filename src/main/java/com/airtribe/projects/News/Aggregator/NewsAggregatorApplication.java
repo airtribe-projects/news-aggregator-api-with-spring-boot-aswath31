2e46f525ed8bab68278878c32dafd889b42cad30
aswath <aswathnarayanan007@gmail.com>
2025-07-03 00:07:54 +0530

    initial setup

---
+++ src/main/java/com/airtribe/projects/News/Aggregator/NewsAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.airtribe.projects.News.Aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airtribe/projects/News/Aggregator/NewsAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airtribe.projects.News.Aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
